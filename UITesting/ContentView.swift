98644b3d665c35d4653b3cadd3655f0f18ce327a
Christian Skorobogatow <christiansko@icloud.com>
2022-08-15 21:10:24 +0200

    Initial Commit

---
+++ UITesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITesting
+//
+//  Created by Christian Skorobogatow on 15/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITesting/UITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingApp.swift
+//  UITesting
+//
+//  Created by Christian Skorobogatow on 15/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
